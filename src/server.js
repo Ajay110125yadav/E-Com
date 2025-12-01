4d2123e74f60bd5503787aeada06e4cbba5eb4d5
Ajay Yadav <ajay@example.com>
2025-12-02 00:36:04 +0530

    Add: simple API

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("DB Error:", err))
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Ecommerce API Working" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
